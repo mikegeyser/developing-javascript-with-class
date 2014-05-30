f0603f5df08455a88fde491ad3d6e4529b61ecb2
Mike Geyser <michaelg@bbd.co.za>
2014-05-30 12:07:32 +0200

    Starting point.

---
+++ vehiclefactory.js
@@ -0,0 +1,26 @@
+﻿(function (app) {
+
+    app.VehicleFactory = {
+        build: function (vehicles) {
+            // Build a random vehicle every two seconds
+            setInterval(function () {
+                var i = Math.floor(Math.random() * vehicles.length);
+                var vehicle = new vehicles[i]();
+
+                // Add text to the DOM
+                var div = $(document.createElement('div'));
+                div.addClass(vehicle.type);
+                div.text(vehicle.drive());
+
+                //$(".parking").prepend(div);
+                div.hide()
+                    .css('opacity', 0.0)
+                    .prependTo('.parking')
+                    .slideDown('slow')
+                    .animate({ opacity: 1.0 });
+
+            }, 2000);
+        }
+    };
+
+})(window.app = window.app || {});
---
+++ vehicles.js
@@ -0,0 +1,52 @@
+﻿(function (app) {
+    // Vehicle (base class)
+    var vehicle = function (type) {
+        this.type = type;
+    };
+
+    vehicle.prototype.drive = function () {
+        return "Vroooom!";
+    };
+
+    app.Vehicle = vehicle;
+
+    // Car
+    var car = function () {
+        app.Vehicle.call(this, "car");
+    };
+
+    car.prototype = new app.Vehicle(); //eww
+
+    car.prototype.drive = function () {
+        return "Nyeeeeeow!";
+    }
+
+    app.Car = car;
+
+    // Truck
+    var truck = function () {
+        app.Vehicle.call(this, "truck");
+    }
+
+    truck.prototype = new app.Vehicle();
+
+    truck.prototype.drive = function () {
+        return app.Vehicle.prototype.drive.apply(this, []);
+    }
+
+    app.Truck = truck;
+
+    // Scooter
+    var scooter = function () {
+        app.Vehicle.call(this, "scooter");
+    }
+
+    scooter.prototype = new app.Vehicle();
+
+    scooter.prototype.drive = function () {
+        return "Meep. Meep.";
+    }
+
+    app.Scooter = scooter;
+
+})(window.app = window.app || {});
